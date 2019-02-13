e1b2db76f0e15f109280932d11dd5c93955e395b
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 19:59:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,6 @@
 require 'pry'
 def consolidate_cart(cart)
   # code here
-
   hash = {}
   i = 0
   array = cart.uniq
@@ -9,22 +8,38 @@ def consolidate_cart(cart)
   while i < array.length
     array[i].each do |key, val|
       hash[key] = val
+      count = 0
       cart.each do |v2|
-        binding.pry
+        if v2 == array[i]
+          count += 1
+        end
+        hash[key][:count] = count
       end
-
     end
     i += 1
   end
-
-
-  binding.pry
-
+  #binding.pry
   hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  checkout_hash = {}
+  cart.each do |item, attributes|
+    if item == coupons[:item]
+      temp = attributes[:count] - coupons[:num]
+      checkout_hash["#{item} W/COUPON"] = {
+        :price => attributes[:price],
+        :clearance => attribute[:clearance],
+        :count => 1
+      }
+    end
+    if temp != 0
+      checkout_hash[item] = attributes
+      checkout_hash[item][:count] = temp
+    end
+  end
+
 end
 
 def apply_clearance(cart)
