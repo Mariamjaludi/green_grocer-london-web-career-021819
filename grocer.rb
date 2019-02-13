acbab3c3d76fc5551659d74d04f8fc042d7aa7ba
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 20:42:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,7 +61,7 @@ end
 def checkout(cart, coupons)
   # code here
   binding.pry
-  cart = consolidate_cart(cart)
+  cart = consolidate_cart(cart: [])
   #binding.pry
   cart = apply_coupons(cart: cart, coupons: coupons)
   cart = apply_clearance(cart: cart)
