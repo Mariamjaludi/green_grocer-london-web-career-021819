9b9ad80954dd436083921e235b4d705c154306b0
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 20:21:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,12 +49,12 @@ end
 def apply_clearance(cart)
   # code here
   cart.collect do |item, attributes|
-    binding.pry
+    
     if attributes[:clearance] == true
       attributes[:price] = attributes[:price] * 0.8
     end
   end
-
+  binding.pry
 end
 
 def checkout(cart, coupons)
